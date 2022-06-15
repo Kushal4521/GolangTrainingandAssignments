840b88de31a8fe9df6852e33758682751299e706
kushal <kushalshah277@gmail.com>
2022-06-15 13:25:47 -0400

    golangprogramme

---
+++ kush.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"PROJECT/training"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, usa")
+	training.Test(5, 6)
+	training.North(23, "brunswick")
+}
---
+++ training/desk.go
@@ -0,0 +1,10 @@
+package training
+
+import "fmt"
+
+func Test(x int, y int) {
+	fmt.Println(x, y)
+}
+func North(a int, b string) {
+	fmt.Println(a, b)
+}
